96cd5c4fabe66046a6367d863badf656b9e2879b
Jordy Crescoli <jordy@alterngo.ch>
2025-01-17 08:57:14 +0100

    fix: remove Livewire Chats component on Chat View page

--- src/Livewire/Chat/View.php
+++ src/Livewire/Chat/View.php
@@ -33,13 +33,6 @@ class View extends Component
 
         return <<<'BLADE'
               <div class="w-full h-[calc(100vh_-_0.0rem)]  flex rounded-lg" >
-                  <div class=" hidden md:grid bg-inherit  dark:bg-inherit  relative w-full h-full md:w-[360px] lg:w-[400px] xl:w-[500px]  shrink-0 overflow-y-auto  ">
-
-            
-                     <livewire:chats/> 
-
-                  </div>
-                  
                   <main  class="  grid  w-full  grow  h-full relative overflow-y-auto"  style="contain:content">
 
                     
