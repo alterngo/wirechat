d3930804be82ee226634166668568ae0831e6f0d
Jordy Crescoli <jordy@alterngo.ch>
2025-01-17 10:39:18 +0100

    fix: display back button on desktop viewports in chat header

--- resources/views/components/chat/header.blade.php
+++ resources/views/components/chat/header.blade.php
@@ -15,7 +15,7 @@
     <div class="  flex  w-full items-center   px-2 py-2   lg:px-4 gap-2 md:gap-5 ">
 
         {{-- Return --}}
-        <a href="{{route(WireChat::indexRouteName())}}" class=" shrink-0 lg:hidden  dark:text-white" id="chatReturn">
+        <a href="{{route(WireChat::indexRouteName())}}" class=" shrink-0 dark:text-white" id="chatReturn">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
