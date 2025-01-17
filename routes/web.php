b08822610462a58c73cea97664139e97678fcfd0
Jordy Crescoli <jordy@alterngo.ch>
2025-01-17 08:58:09 +0100

    fix: remove chats endpoint

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ use Namu\WireChat\Livewire\Chat\View;
 Route::middleware(config('wirechat.routes.middleware'))
     ->prefix(config('wirechat.routes.prefix'))
     ->group(function () {
-        Route::get('/', Index::class)->name('chats');
+        //Route::get('/', Index::class)->name('chats');
         Route::get('/{conversation_id}', View::class)->name('chat');
     });
